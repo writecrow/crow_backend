bb2c4453118210586d7822dc21f1492d3fbaaea7
Mark Fullmer <mfullmer@gmail.com>
2021-08-10 19:44:21 -0500

    32 - Support import of texts created by multiple students (#33)
    * Update version of writecrow/tag_converter
    
    * Capture latest configuration from live environment
    
    * Add system advisory configuration
    
    * Allow multiple values for gender, country, L1, semester, year, and year in school
    
    * Support multiple student IDs
    
    * Update text form display to match offline corpus structure
    
    * Handle import of multiple student values
    
    * Update Psr/Log/AbstractLogger syntax
    
    * Update assignment map
    
    * Ensure that texts with multiple values for any header can be properly imported
    
    Co-authored-by: Mark Fullmer <jfullmer@austin.utexas.edu>

--- profiles/corpus/modules/corpus_importer/src/ImporterHelper.php
+++ profiles/corpus/modules/corpus_importer/src/ImporterHelper.php
@@ -29,28 +29,22 @@ class ImporterHelper {
     $messages = [];
     // Loop through each of the taxonomies defined in the backend.
     foreach (ImporterMap::$corpusTaxonomies as $name => $machine_name) {
-
       // Perform data mapping & cleanup operations on metadata.
-      $text = self::mapMetadata($text, $name, $machine_name);
+      $text[$name] = self::mapMetadata($text, $name, $machine_name);
 
-      if (!in_array($name, array_keys($text))) {
-        // Skip headers that aren't known to the backend.
-        $messages[] = 'Unknown header: ' . $name;
-        continue;
-      }
-      if (in_array($text[$name], ImporterMap::$notAvailableValues)) {
-        // Skip various N/A values. These will be reported later on.
-        continue;
-      }
       if (empty($text[$name])) {
         continue;
       }
-
+    }
+    foreach (ImporterMap::$corpusTaxonomies as $name => $machine_name) {
       if (is_array($text[$name])) {
         // Save multiple values.
         $tids = [];
         foreach ($text[$name] as $term) {
           $term_data = self::getOrCreateTidFromName($term, $machine_name, $options);
+          if (!$term_data) {
+            continue;
+          }
           $tids[] = $term_data['tid'];
           // Report any terms newly created.
           if (!empty($term_data['message'])) {
@@ -62,6 +56,9 @@ class ImporterHelper {
       else {
         // Save a single value.
         $term_data = self::getOrCreateTidFromName($text[$name], $machine_name, $options);
+        if (!$term_data) {
+          continue;
+        }
         $fields[$machine_name] = $term_data['tid'];
         // Report any terms newly created.
         if (!empty($term_data['message'])) {
@@ -90,6 +87,12 @@ class ImporterHelper {
    */
   public static function getOrCreateTidFromName($label, $vocabulary, $options) {
     $output = [];
+
+    // Skip N/A values.
+    if (in_array($label, ImporterMap::$notAvailableValues)) {
+      return FALSE;
+    }
+
     $output['tid'] = self::getTidByName($label, $vocabulary);
     if ($output['tid'] == 0) {
       $output['message'] = 'New ' . $vocabulary . ' created: ' . $label;
@@ -97,7 +100,7 @@ class ImporterHelper {
         self::createTerm($label, $vocabulary);
       }
       $output['tid'] = self::getTidByName($label, $vocabulary);
-    } 
+    }
     return $output;
   }
 
@@ -112,99 +115,90 @@ class ImporterHelper {
    *   The mapped machine name to the backend taxonomy (e.g., 'semester').
    *
    * @return array
-   *   The headers, as an array, with mapped values (e.g., "SL" is now "Syllabus").
+   *   The header, as an array, with mapped values (e.g., "SL" is now "Syllabus").
    */
   public static function mapMetadata(array $text, $name, $machine_name) {
-    if (in_array($machine_name, ['program', 'college'])) {
-      if (is_string($text[$name])) {
-        if ($text[$name] == "NA") {
-          // Leave "NA" program/college values as-is.
-          return $text;
-        }
-        $multiples = preg_split("/\s?;\s?/", $text[$name]);
-        if (isset($multiples[1])) {
-          array_push($multiples, $text[$name]);
-          $text[$name] = $multiples;
-        }
-        else {
-          $text[$name] = [$text[$name]];
-        }
+    // Convert all string values to associative array.
+    if (is_string($text[$name])) {
+      $text[$name] = [$text[$name]];
+    }
+    foreach ($text[$name] as $key => $value) {
+
+      switch ($machine_name) {
+        case 'assignment':
+          if (in_array($value, array_keys(ImporterMap::$assignments))) {
+            $text[$name][$key] = ImporterMap::$assignments[$value];
+          }
+          break;
+
+        case 'college':
+          $institution = $text['Institution'];
+          if (in_array($value, array_keys(ImporterMap::$collegeGeneral))) {
+            // The text metadata uses a college acronymn.
+            // The college name is shared across institutions.
+            $text[$name][$key] = ImporterMap::$collegeGeneral[$value];
+          }
+          elseif (isset(ImporterMap::$collegeSpecific[$institution]) && in_array($value, array_keys(ImporterMap::$collegeSpecific[$institution]))) {
+            // The text metadata uses a college acronymn.
+            // The college name is institution-specific.
+            $text[$name][$key] = ImporterMap::$collegeSpecific[$institution][$value];
+          }
+          break;
+
+        case 'course':
+          if (in_array($value, array_keys(ImporterMap::$courseFixes))) {
+            $text[$name][$key] = ImporterMap::$courseFixes[$value];
+          }
+          break;
+
+        case 'country':
+          if (in_array($value, array_keys(ImporterMap::$countryFixes))) {
+            $text[$name][$key] = ImporterMap::$countryFixes[$value];
+          }
+          $text[$name][$key] = CountryCodeConverter::convert($text[$name][$key], 'name');
+          break;
+
+        case 'draft':
+          if (in_array($value, array_keys(ImporterMap::$draftFixes))) {
+            $text[$name][$key] = ImporterMap::$draftFixes[$value];
+          }
+          break;
+
+        case 'gender':
+          if (in_array($value, array_keys(ImporterMap::$genderFixes))) {
+            $text[$name][$key] = ImporterMap::$genderFixes[$value];
+          }
+          break;
+
+        case 'institution':
+          if (in_array($value, array_keys(ImporterMap::$institutionFixes))) {
+            $text[$name][$key] = ImporterMap::$institutionFixes[$value];
+          }
+          // Purdue texts only.
+          if ($text['Institution'] == 'Purdue University' && $value == 'LR') {
+            $text[$name][$key] = 'SY';
+          }
+          break;
+
+        case 'program':
+          if (in_array($value, array_keys(ImporterMap::$programFixes))) {
+            $text[$name][$key] = ImporterMap::$programFixes[$value];
+          }
+          break;
+
+        case 'semester':
+          if (in_array($value, array_keys(ImporterMap::$semesters))) {
+            $text[$name][$key] = ImporterMap::$semesters[$value];
+          }
+          break;
+      }
+      // Unset any N/A values.
+      if (in_array($value, ImporterMap::$notAvailableValues)) {
+        // Skip various N/A values. These will be reported later on.
+        unset($text[$name][$key]);
       }
     }
-    if (in_array($machine_name, ['institution']) && empty($text['Institution'])) {
-      $text['Institution'] = 'Purdue University';
-    }
-    if (in_array($machine_name, ['gender']) && $text['Gender'] == 'G') {
-      $text['Gender'] = 'M';
-    }
-    // Purdue texts
-    if ($text['Institution'] == 'Purdue University' && $text['Assignment'] == 'LR') {
-      $text['Assignment'] = 'SY';
-    }
-    if ($machine_name == 'assignment') {
-      $assignment_code = $text['Assignment'];
-      $text['Assignment'] = ImporterMap::$assignments[$assignment_code];
-    }
-    // Standardize draft names.
-    if ($machine_name == 'draft') {
-      if (in_array($text[$name], array_keys(ImporterMap::$draftFixes))) {
-        $code = $text[$name];
-        $text[$name] = ImporterMap::$draftFixes[$code];
-      }
-    }
-    // Standardize course names.
-    if ($machine_name == 'course') {
-      if (in_array($text[$name], array_keys(ImporterMap::$courseFixes))) {
-        $code = $text[$name];
-        $text[$name] = ImporterMap::$courseFixes[$code];
-      }
-    }
-    // Standardize semesters.
-    if ($machine_name == 'semester') {
-      if (in_array($text[$name], array_keys(ImporterMap::$semesters))) {
-        $code = $text[$name];
-        $text[$name] = ImporterMap::$semesters[$code];
-      }
-    }
-    // Convert country IDs to readable names.
-    if ($machine_name == 'country') {
-      if (in_array($text[$name], array_keys(ImporterMap::$countryFixes))) {
-        $code = $text[$name];
-        $text[$name] = ImporterMap::$countryFixes[$code];
-      }
-      $text[$name] = CountryCodeConverter::convert($text[$name], 'name');
-    }
-    if ($machine_name == 'college') {
-      $instititution = $text['Institution'];
-      $colleges = $text['College'];
-      $text['College'] = [];
-      foreach ($colleges as $college) {
-        if (in_array($college, array_keys(ImporterMap::$collegeGeneral))) {
-          // The text metadata uses a college acronymn.
-          // The college name is shared across institutions.
-          $text['College'][] = ImporterMap::$collegeGeneral[$college];
-        }
-        elseif (in_array($college, array_values(ImporterMap::$collegeGeneral))) {
-          // The text metadata provides the actual college name.
-          // The college name is shared across institutions.
-          $text['College'][] = $college;
-        }
-        elseif (isset(ImporterMap::$collegeSpecific[$instititution]) && in_array($college, array_keys(ImporterMap::$collegeSpecific[$instititution]))) {
-          // The text metadata uses a college acronymn.
-          // The college name is institution-specific.
-          $text['College'][] = ImporterMap::$collegeSpecific[$instititution][$college];
-        }
-        elseif (isset(ImporterMap::$collegeSpecific[$instititution]) && in_array($college, array_values(ImporterMap::$collegeSpecific[$instititution]))) {
-          // The text metadata provides the actual college name.
-          // The college name is institution-specific.
-          $text['College'][] = $college;
-        }
-        else {
-          $text['College'][] = $college;
-        }
-      }
-    }
-    return $text;
+    return $text[$name];
   }
 
   /**
