abbd6bd2a0dd447a5c5b3b055a9e87db49176216
Mark Fullmer <mfullmer@gmail.com>
2022-01-02 15:49:01 -0600

    Add basic scaffolding

--- profiles/corpus/tests/src/Functional/BaseLoad.php
+++ profiles/corpus/tests/src/Functional/BaseLoad.php
@@ -1,3 +1,5 @@
+<?php
+
 namespace Drupal\Tests\corpus\Functional;
 
 use Drupal\Tests\BrowserTestBase;
@@ -8,4 +10,38 @@ use Drupal\Tests\BrowserTestBase;
  * @group rules_ui
  */
 class BaseLoad extends BrowserTestBase {
+
+
+  /**
+   * Use the 'corpus' installation profile.
+   *
+   * @var string
+   */
+  protected $profile = 'corpus';
+
+  /**
+   * Specify the theme to be used in testing.
+   *
+   * @var string
+   */
+  protected $defaultTheme = 'corpus_theme';
+
+   /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+  }
+
+ /**
+   * Tests that the search results return the expected JSON payload.
+   */
+  public function testSearchResults() {
+    $account = $this->drupalCreateUser(['administer content']);
+    $this->drupalLogin($account);
+
+    $this->drupalGet('admin/content');
+    $this->assertSession()->statusCodeEquals(200);
+
+  }
 }
