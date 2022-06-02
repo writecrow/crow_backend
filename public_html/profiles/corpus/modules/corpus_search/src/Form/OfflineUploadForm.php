5d6769aa9569e51c608da0b408ba00f237a7be09
Mark Fullmer <mfullmer@gmail.com>
2022-06-02 09:37:10 -0700

    Add offline option (#49)
    * Add offline option
    
    * Remove duplicate class
    
    * Re-allow .htaccess to be scaffolded

---
+++ public_html/profiles/corpus/modules/corpus_search/src/Form/OfflineUploadForm.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\corpus_search\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Entity\File;
+
+/**
+ * Allow authorized users to upload a single zip file of the corpus.
+ *
+ * @package Drupal\corpus_search\Form
+ */
+class OfflineUploadForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'corpus_search.offline_upload_form',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'offline_upload_form';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $fid = \Drupal::state()->get('offline_file_id');
+    $form['file'] = [
+      '#type' => 'managed_file',
+      '#upload_location' => 'private://',
+      '#multiple' => FALSE,
+      '#description' => $this->t('Allowed extensions: zip'),
+      '#upload_validators' => [
+        'file_validate_extensions' => ['zip'],
+      ],
+      '#default_value' => [$fid] ?? 0,
+      '#title' => $this->t('Upload a zip file'),
+    ];
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save uploaded file to system'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+    $field = $form_state->getValue('file');
+    if (!$field[0]) {
+      $form_state->setErrorByName('file', $this->t("Upload failed. Please try again."));
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $field = $form_state->getValue('file');
+    $file = File::load($field[0]);
+    // This will set the file status to 'permanent' automatically.
+    \Drupal::service('file.usage')->add($file, 'corpus_search', 'file', $file->id());
+
+    \Drupal::state()->set('offline_file_id', $file->id());
+  }
+
+}
---
+++ public_html/profiles/corpus/modules/corpus_search/src/Plugin/rest/resource/OfflineCorpusExport.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Drupal\corpus_search\Plugin\rest\resource;
+
+
+use Drupal\rest\Plugin\ResourceBase;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\file\Entity\File;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Provides a resource to get corpus search results.
+ *
+ * This is modeled on https://www.drupal.org/project/drupal/issues/2884721.
+ *
+ * @RestResource(
+ *   id = "offline_corpus_export",
+ *   label = @Translation("Offline corpus export"),
+ *   uri_paths = {
+ *     "canonical" = "/corpus/offline"
+ *   }
+ * )
+ */
+class OfflineCorpusExport extends ResourceBase {
+
+  /**
+   * A curent user instance.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The request.
+   *
+   * @var \Symfony\Component\HttpFoundation\Request
+   */
+  protected $currentRequest;
+
+  /**
+   * Constructs a Drupal\rest\Plugin\ResourceBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   The current user instance.
+   * @param Symfony\Component\HttpFoundation\Request $current_request
+   *   The current request.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger, AccountProxyInterface $current_user, Request $current_request) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->currentUser = $current_user;
+    $this->currentRequest = $current_request;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('example_rest'),
+      $container->get('current_user'),
+      $container->get('request_stack')->getCurrentRequest()
+    );
+  }
+
+  /**
+   * Responds to GET requests.
+   *
+   * @return \Drupal\rest\ResourceResponse
+   *   The HTTP response object.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
+   *   Throws exception expected.
+   */
+  public function get() {
+    $data = '';
+    $fid = \Drupal::state()->get('offline_file_id');
+    $file = File::load($fid);
+    if ($file) {
+      $data = file_get_contents($file->getFileUri());
+    }
+    $response = new Response();
+    $response->headers->set('Content-Type', 'application/zip');
+    $response->headers->set('Content-Disposition', 'attachment; filename="crow_corpus.zip');
+    $response->setContent($data);
+    return $response;
+  }
+
+}
