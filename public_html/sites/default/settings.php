0929755950c4bb4779359945c06d95b0d59f438d
Mark Fullmer <mfullmer@gmail.com>
2022-09-25 09:26:03 -0700

    Ignore phpmailer_smtp from config

--- public_html/sites/default/settings.php
+++ public_html/sites/default/settings.php
@@ -57,7 +57,7 @@ $settings['config_sync_directory'] = $app_root . '/../config';
  * useful in a configuration file for a vhost or directory, rather than
  * the default settings.php.
  */
-$settings['config_exclude_modules'] = ['smtp'];
+$settings['config_exclude_modules'] = ['smtp', 'phpmailer_smtp.settings'];
 
 /**
  * Fast 404 pages:
